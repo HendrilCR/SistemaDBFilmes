5705d3afffbe58428e5a29564cf0ab7926ec4b22
HendrilCR <hendrilcr2004@gmail.com>
2025-02-14 15:48:46 -0300

    Main

--- src/main/java/com/example/demo/Controllers/FilmeController.java
+++ src/main/java/com/example/demo/Controllers/FilmeController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controllers;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.Models.Filme;
@@ -18,10 +19,16 @@ public class FilmeController{
     }
 
     @GetMapping("/inicio")
-    public String inicio(){
+    public String inicio(Model model){
         var filmes = (List<Filme>) filmeService.findAll();
         System.out.println(filmes); 
         System.out.println(filmes.get(0).getNome());
+
+        String texto = "MANOO";
+        Filme filme = filmes.get(0);
+
+        model.addAttribute("texto", texto);
+        model.addAttribute("filme", filme);
         return "inicio";
     }
 }
